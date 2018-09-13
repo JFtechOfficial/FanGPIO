047da33b1cc37094349bbb646a6916931e7867b9
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-09-13 13:20:27 +0200

    Update FanGPIO.c

--- FanGPIO.c
+++ FanGPIO.c
@@ -14,27 +14,27 @@ int main (int sleepTime, int maxTemp, int cutoffTemp)
   double step = (70 - maxTemp) * 1000 / 3;
   for (;;)
   {
-	usleep(sleepTime * 1000);
 	FILE *temperatureFile;
-		double T;
-		temperatureFile = fopen ("/sys/class/thermal/thermal_zone0/temp", "r");
-		fscanf (temperatureFile, "%lf", &T);
-		if (T < cutoffTemperature){
-			softPwmWrite (1, 0);
-		}
-		if (T >= maxTemperature && T < maxTemperature + step){
-			softPwmWrite (1, 25);
-		}
-		if (T >= maxTemperature + step && T < maxTemperature + (step * 2)){
-			softPwmWrite (1, 50);
-		}
-		if (T >= maxTemperature + (step * 2) && T < 70000){
+	double T;
+	temperatureFile = fopen ("/sys/class/thermal/thermal_zone0/temp", "r");
+	fscanf (temperatureFile, "%lf", &T);
+	if (T < cutoffTemperature){
+		softPwmWrite (1, 0);
+	}
+	if (T >= maxTemperature && T < maxTemperature + step){
+		softPwmWrite (1, 25);
+	}
+	if (T >= maxTemperature + step && T < maxTemperature + (step * 2)){
+		softPwmWrite (1, 50);
+	}
+	if (T >= maxTemperature + (step * 2) && T < 70000){
 			softPwmWrite (1, 75);
-		}
-		if (T >= 70000){
-			softPwmWrite (1, 100);
-		}
-		fclose (temperatureFile);
+	}
+	if (T >= 70000){
+		softPwmWrite (1, 100);
+	}
+	fclose (temperatureFile);
+	usleep(sleepTime * 1000);  
   }
   return 0;
 }
